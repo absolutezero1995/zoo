c4eb23747cb4151a0d4c906467619d4037859771
absolutezero1995 <sokolovmaksim2391@gmail.com>
2024-01-23 14:48:45 -0100

    test

--- backend/db/config/database.js
+++ backend/db/config/database.js
@@ -7,4 +7,18 @@ module.exports = {
     "host": DB_HOST,
     "dialect": "postgres"
   },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  }
 }
