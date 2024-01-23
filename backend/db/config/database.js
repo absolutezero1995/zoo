e01e469c396a4a2e733a313586e8d7b3ac92998b
absolutezero1995 <sokolovmaksim2391@gmail.com>
2024-01-23 15:21:14 -0100

    qqqq

--- backend/db/config/database.js
+++ backend/db/config/database.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME || DB_USERNAME,
+    "password": process.env.DB_PASSWORD || DB_PASSWORD,
+    "database": process.env.DB_NAME || DB_NAME, 
+    "host": process.env.DB_HOST || DB_HOST,
     "dialect": "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  }
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": process.env.DB_USERNAME,
+  //   "password": process.env.DB_PASSWORD,
+  //   "database": process.env.DB_NAME,
+  //   "host": process.env.DB_HOST,
+  //   "dialect": "postgres"
+  // }
 }
