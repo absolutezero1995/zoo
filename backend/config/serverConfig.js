f42e4019a3a02ff7949f11389d205602ae9952ef
absolutezero1995 <sokolovmaksim2391@gmail.com>
2024-01-23 14:07:06 -0100

    vt test 8

--- backend/config/serverConfig.js
+++ backend/config/serverConfig.js
@@ -7,7 +7,7 @@ const sessionConfig = require("../config/sessionConfig");
 const cors = require('cors');
 
 const corsOption = {
-  origin: 'http://localhost/*',
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200
 }
